7a0c00a8caef68638959cb11e85a9e8ee2558918
William Morais Paim <willmpaim@gmail.com>
2022-11-23 20:13:26 -0300

    atualizando os metodos de boas praticas

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,27 +1,18 @@
 export class Negociacao {
-    private _data: Date;
-    private _quantidade: number;
-    private _valor: number;
+   
+    constructor(
+        private _data: Date,
+        public readonly _quantidade: number,
+        public readonly _valor: number
+        ) {}
 
-    constructor(data: Date, quantidade: number, valor: number) {
-        this._data = data;
-        this._quantidade = quantidade;
-        this._valor = valor;
+  
+    get volume(): number {
+        return this._quantidade * this._valor;
     }
 
     get data(): Date {
-        return this._data;
-    }
-
-    get quantidade(): number {
-        return this._quantidade;
-    }
-
-    get valor(): number {
-        return this._valor;
-    }
-
-    get volume(): number {
-        return this._quantidade * this._valor;
+        const data = new Date(this._data.getTime());
+        return data;
     }
 }
